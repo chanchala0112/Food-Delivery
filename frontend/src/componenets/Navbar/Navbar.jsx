bf7a6e6cec9506db78fe7336ba8889c734d9067d
Chanchala Jeewanthi Jayalath <jeewanthichanchala321@gmail.com>
2025-07-29 10:48:13 +0530

    update nav bar

--- frontend/src/componenets/Navbar/Navbar.jsx
+++ frontend/src/componenets/Navbar/Navbar.jsx
@@ -3,14 +3,17 @@ import './Navbar.css'
 import { assets } from '../../assets/assets'
 
 const Navbar = () => {
+
+  
+
   return (
     <div className='navbar'>
         <img src={assets.logo} alt="" className="logo" />
         <ul className="navbar-menu">
-          <li>Home</li>
-          <li>Menu</li>
-          <li>Mobile-app</li>
-          <li>Contact Us</li>
+          <li >Home</li>
+          <li >Menu</li>
+          <li >Mobile-app</li>
+          <li >Contact Us</li>
         </ul>
         <div className="navbar-right">
           <img src={assets.search_icon} alt="" />
