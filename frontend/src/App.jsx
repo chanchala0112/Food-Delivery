0f8cb077522a64525a8e9c6d6b355ff0837bb37c
Chanchala Jeewanthi Jayalath <jeewanthichanchala321@gmail.com>
2025-07-31 14:05:48 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Navbar from './componenets/Navbar/Navbar'
-
+import { Routes, Route } from 'react-router-dom'  
+import Home from './pages/Home/Home.jsx'
+import Cart from './pages/Cart/Cart.jsx'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder.jsx'  
 
 const App = () => {
   return (
     <div className='app'>
-      <Navbar/>
+      <Navbar />
+      
     </div>
   )
 }
